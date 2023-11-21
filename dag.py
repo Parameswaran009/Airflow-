f1fed85bf998e1a0d39451663fb173158bfcc021
Parameswaran009 <parameswarankrishnan36@gmail.com>
2023-11-21 17:38:57 +0530

    modifed dag file

--- dag.py
+++ dag.py
@@ -1,5 +1,3 @@
-# my_airflow_dag.py
-
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
@@ -11,21 +9,22 @@ def fetch_code_from_s3(bucket_name, object_key, local_path):
     s3 = boto3.client('s3')
     s3.download_file(bucket_name, object_key, local_path)
 
+def execute_python_code(local_path):
+    # Execute the downloaded Python code
+    exec(open(local_path).read(), globals(), locals())
+    print("Python code executed successfully")
+
 def my_python_function(**kwargs):
-    try:
-        # Replace 'mwaa-environmentbucket-beynhcbcqflf' and 's3://mwaa-environmentbucket-beynhcbcqflf/dag.py'
-        # with your S3 bucket and path
-        fetch_code_from_s3('mwaa-environmentbucket-beynhcbcqflf', 's3://mwaa-environmentbucket-beynhcbcqflf/dag.py', '/home/ubuntu/airflow/code/dag.py')
+    # Replace 'your-s3-bucket' and 'path/to/your/code.py' with your S3 bucket and path
+    s3_bucket = 'mwaa-environmentbucket-beynhcbcqflf'
+    s3_key = 's3://mwaa-environmentbucket-beynhcbcqflf/dag.py'
+    local_path = '/home/ubuntu/airflow/code/dag.py'
+    
+    # Fetch code from S3
+    fetch_code_from_s3(s3_bucket, s3_key, local_path)
 
-        # Replace '/home/ubuntu/airflow/code/' with your desired local path
-        local_path = '/home/ubuntu/airflow/code/'
-
-        # Execute the downloaded Python code
-        exec(open(os.path.join(local_path, 'dag.py')).read(), globals(), locals())
-        print("Python code executed successfully")
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        raise  # This will mark the task as failed
+    # Execute the downloaded Python code
+    execute_python_code(local_path)
 
 default_args = {
     'owner': 'airflow',
@@ -36,17 +35,8 @@ default_args = {
 
 with DAG('my_airflow_dag', default_args=default_args, schedule_interval=None) as dag:
     start_task = DummyOperator(task_id='start_task')
-
-    python_task = PythonOperator(
-        task_id='python_task',
-        python_callable=my_python_function,
-        provide_context=True,
-        retries=0,  # Set retries to 0 to disable automatic retries
-    )
-
+    python_task = PythonOperator(task_id='python_task', python_callable=my_python_function, provide_context=True)
     end_task = DummyOperator(task_id='end_task')
 
     start_task >> python_task >> end_task
-
-
 
