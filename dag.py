c10781b62606fa767fda51cf826700a90faef46a
Parameswaran009 <parameswarankrishnan36@gmail.com>
2023-11-21 16:10:24 +0530

    modify day file

--- dag.py
+++ dag.py
@@ -1,3 +1,5 @@
+# my_airflow_dag.py
+
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
@@ -10,15 +12,20 @@ def fetch_code_from_s3(bucket_name, object_key, local_path):
     s3.download_file(bucket_name, object_key, local_path)
 
 def my_python_function(**kwargs):
-    # Replace 'your-s3-bucket' and 'path/to/your/code.py' with your S3 bucket and path
-    fetch_code_from_s3('mwaa-environmentbucket-beynhcbcqflf', 's3://mwaa-environmentbucket-beynhcbcqflf/dag.py', '/home/ubuntu/airflow/code/code.py')
-    
-    # Replace '/var/lib/airflow/code/' with your desired local path
-    local_path = '/home/ubuntu/airflow/code/'
-    
-    # Execute the downloaded Python code
-    exec(open(os.path.join(local_path, 'code.py')).read(), globals(), locals())
-    print("Python code executed successfully")
+    try:
+        # Replace 'mwaa-environmentbucket-beynhcbcqflf' and 's3://mwaa-environmentbucket-beynhcbcqflf/dag.py'
+        # with your S3 bucket and path
+        fetch_code_from_s3('mwaa-environmentbucket-beynhcbcqflf', 's3://mwaa-environmentbucket-beynhcbcqflf/dag.py', '/home/ubuntu/airflow/code/code.py')
+
+        # Replace '/home/ubuntu/airflow/code/' with your desired local path
+        local_path = '/home/ubuntu/airflow/code/'
+
+        # Execute the downloaded Python code
+        exec(open(os.path.join(local_path, 'code.py')).read(), globals(), locals())
+        print("Python code executed successfully")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        raise  # This will mark the task as failed
 
 default_args = {
     'owner': 'airflow',
@@ -29,9 +36,15 @@ default_args = {
 
 with DAG('my_airflow_dag', default_args=default_args, schedule_interval=None) as dag:
     start_task = DummyOperator(task_id='start_task')
-    python_task = PythonOperator(task_id='python_task', python_callable=my_python_function, provide_context=True)
+
+    python_task = PythonOperator(
+        task_id='python_task',
+        python_callable=my_python_function,
+        provide_context=True,
+        retries=0,  # Set retries to 0 to disable automatic retries
+    )
+
     end_task = DummyOperator(task_id='end_task')
 
     start_task >> python_task >> end_task
-
 
