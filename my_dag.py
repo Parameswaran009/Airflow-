4c8c4f14927990091d86a399f8dba400387aa813
Parameswaran009 <parameswarankrishnan36@gmail.com>
2023-11-22 15:46:25 +0530

    modified dag code

--- my_dag.py
+++ my_dag.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2023, 1, 1),
-    'retries': 0,
+    'retries': 1,
     'retry_delay': 0,
 }
 
